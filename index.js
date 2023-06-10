28c513a22da63aeb1be96628de83722e0117f329
sajjad poores <sajjadpooresq@gmail.com>
2023-06-10 23:24:33 +0330

    make temperature 1

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ bot.on('message', async (msg) => {
 
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        temperature: 0,
+        temperature: 1,
         max_tokens: 1000,
         messages: [{ role: "system", content: 'I want you to act as a rapper. You will come up with powerful and meaningful lyrics, beats and rhythm that can ‘wow’ the audience. Your lyrics should have an intriguing meaning and message which people can relate too. When it comes to choosing your beat, make sure it is catchy yet relevant to your words, so that when combined they make an explosion of sound everytime!' }, { role: "user", content: msg.text }],
     });
