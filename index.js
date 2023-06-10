a5bd58c39c9344f644d9fe9e138b6d902edd94bc
sajjad poores <sajjadpooresq@gmail.com>
2023-06-10 23:40:43 +0330

    fix(index): fix keyboard and modemap texts

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ bot.on('message', async (msg) => {
     if (msg.text === "/mode") {
         bot.sendMessage(msg.chat.id, "Who you want me to act like?", {
             "reply_markup": {
-                "keyboard": [["Rapper"], ["angry man"]]
+                "keyboard": [["Rapper"], ["Angry man"], ["Normal person"]]
             }
         });
     }
-    else if (msg.text === "angry") {
-        mode = "angry";
+    else if (msg.text === "Angry man") {
+        mode = "Angry man";
         bot.sendMessage(chatId, 'Robot is in angry mode now');
     }
-    else if (msg.text === "rapper") {
-        mode = "rapper";
+    else if (msg.text === "Rapper") {
+        mode = "Rapper";
         bot.sendMessage(chatId, 'Robot is in rapper mode now');
     }
-    else if (msg.text === "normal") {
+    else if (msg.text === "Normal person") {
         mode = "normal";
         bot.sendMessage(chatId, 'Robot is in normal mode now');
     }
@@ -45,9 +45,9 @@ bot.on('message', async (msg) => {
 
 
         const modeMap = {
-            normal: 'you are a wise person, answer user text',
-            rapper: 'I want you to act as a rapper. You will come up with powerful and meaningful lyrics, beats and rhythm that can ‘wow’ the audience. Your lyrics should have an intriguing meaning and message which people can relate too. When it comes to choosing your beat, make sure it is catchy yet relevant to your words, so that when combined they make an explosion of sound everytime!',
-            angry: 'I want you to act like a very angry person that knows everything but response in a very angry way.'
+            "Normal person": 'you are a wise person, answer user text',
+            "Rapper": 'I want you to act as a rapper. You will come up with powerful and meaningful lyrics, beats and rhythm that can ‘wow’ the audience. Your lyrics should have an intriguing meaning and message which people can relate too. When it comes to choosing your beat, make sure it is catchy yet relevant to your words, so that when combined they make an explosion of sound everytime!',
+            "Angry man": 'I want you to act like a very angry person that knows everything but response in a very angry way.'
         }
         const completion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
