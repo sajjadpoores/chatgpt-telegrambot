02d332d3be01eab07c5f266b6e894223629fbfe6
sajjad poores <sajjadpooresq@gmail.com>
2023-06-10 22:54:51 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_KEY;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+console.log('the chatgpt telegram bot is running....')
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
