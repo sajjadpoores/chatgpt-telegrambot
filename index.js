95700ac7cdb549b031451469e1362ecde390235c
sajjad poores <sajjadpooresq@gmail.com>
2023-06-10 23:37:06 +0330

    add modes to bot

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+let mode = 'normal';
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
@@ -9,25 +9,58 @@ const token = process.env.TELEGRAM_KEY;
 const bot = new TelegramBot(token, { polling: true });
 
 console.log('the chatgpt telegram bot is running....')
+
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
 
+    if (msg.text === "angry") {
+        mode = "angry";
+        bot.sendMessage(chatId, 'Robot is in angry mode now');
+    }
+    else if (msg.text === "rapper") {
+        mode = "rapper";
+        bot.sendMessage(chatId, 'Robot is in rapper mode now');
+    }
+    else if (msg.text === "normal") {
+        mode = "normal";
+        bot.sendMessage(chatId, 'Robot is in normal mode now');
+    }
+    else {
 
-    const { Configuration, OpenAIApi } = require("openai");
 
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_KEY,
+        const { Configuration, OpenAIApi } = require("openai");
+
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+        bot.sendChatAction(chatId, 'typing')
+
+
+        const modeMap = {
+            normal: 'you are a wise person, answer user text',
+            rapper: 'I want you to act as a rapper. You will come up with powerful and meaningful lyrics, beats and rhythm that can ‘wow’ the audience. Your lyrics should have an intriguing meaning and message which people can relate too. When it comes to choosing your beat, make sure it is catchy yet relevant to your words, so that when combined they make an explosion of sound everytime!',
+            angry: 'I want you to act like a very angry person that knows everything but response in a very angry way.'
+        }
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            temperature: 1,
+            max_tokens: 1000,
+            messages: [{ role: "system", content: modeMap[mode] }, { role: "user", content: msg.text }],
+        });
+
+        console.log(completion.data.choices[0].message)
+        bot.sendMessage(chatId, completion.data.choices[0].message.content);
+    }
+
+});
+
+bot.onText(/\/mode/, (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Who you want me to act lie?", {
+        "reply_markup": {
+            "keyboard": [["Rapper"], ["angry man"]]
+        }
     });
-    const openai = new OpenAIApi(configuration);
-    bot.sendChatAction(chatId, 'typing')
 
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        temperature: 1,
-        max_tokens: 1000,
-        messages: [{ role: "system", content: 'I want you to act as a rapper. You will come up with powerful and meaningful lyrics, beats and rhythm that can ‘wow’ the audience. Your lyrics should have an intriguing meaning and message which people can relate too. When it comes to choosing your beat, make sure it is catchy yet relevant to your words, so that when combined they make an explosion of sound everytime!' }, { role: "user", content: msg.text }],
-    });
-
-    console.log(completion.data.choices[0].message)
-    bot.sendMessage(chatId, completion.data.choices[0].message.content);
 });
