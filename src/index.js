5f3c044390ce07664aad03e23d27bf9d5a71c525
sajjad poores <sajjadpooresq@gmail.com>
2023-06-21 21:11:49 +0330

    fix problem of not being connected to db and calling users.find

--- src/index.js
+++ src/index.js
@@ -12,15 +12,17 @@ const bot = new TelegramBot(process.env.TELEGRAM_KEY, {polling: true});
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB...'))
+    .then(() => {
+        console.log('Connected to MongoDB...')
+        bot.on('message', (msg) => telegramController.handleMessage(bot, msg));
+        
+        app.use((err, req, res, next) => {
+            console.error(err.stack);
+            res.status(500).send('Something went wrong!');
+        });
+    })
     .catch(err => console.error('Could not connect to MongoDB...'));
 
-bot.on('message', (msg) => telegramController.handleMessage(bot, msg));
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
